042d2ac13087ca2120a99af1fa7a37bc8ad994ff
murumba631 <m.arnold1738@gmail.com>
2020-07-25 21:36:02 +0300

    add fadein and fadeout functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#submit form").submit(function() {
+    $("#submit").submit(function() {
         var name = document.getElementById("userName");
 
         event.preventDefault();
@@ -7,3 +7,27 @@ $(document).ready(function() {
     alert("Thank you " + name + " for your response.");
 });
 
+$(document).ready(function(){
+    $(".clickable1").click(function(){
+        $("#design").fadeOut();
+    });
+    $(".clickable1").click(function(){
+        $("#des-paragraph").fadeIn(1500);
+    });
+});
+$(document).ready(function(){
+    $(".clickable2").click(function(){
+        $("#development").fadeOut();
+    });
+    $(".clickable2").click(function(){
+        $("#dev-paragraph").fadeIn(1500);
+    });
+});
+$(document).ready(function(){
+    $(".clickable3").click(function(){
+        $("#product").fadeOut();
+    });
+    $(".clickable3").click(function(){
+        $("#product-paragraph").fadeIn(1500);
+    });
+});
